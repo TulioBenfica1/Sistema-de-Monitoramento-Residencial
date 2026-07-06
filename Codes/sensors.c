0921edad70b3ed7ae89a358907ff51968d061078
Lavínia Balthazar <lavinia.balthazar@estudante.ufjf.br>
2026-07-06 08:03:16 -0300

    fix: sem comentarios

--- Codes/sensors.c
+++ Codes/sensors.c
@@ -4,22 +4,22 @@ void SensorsInit(void) {
     DDRB = 0x00; // Configura PORTB como entrada
 }
 
-SystemState SensorsUpdate(void) {
+void SensorsUpdate(void) {
     unsigned char flame = ~(PINB & (1 << PB0)); 
     unsigned char presence = (PINB & (1 << PB1)); 
     unsigned char vibration = (PINB & (1 << PB2));
 
     if (flame) {
-        return SystemSetState(ST_FLAME);
+        SystemSetState(ST_FLAME);
     }
     else if (presence && vibration) {
-        return SystemSetState(ST_INVASION);
+        SystemSetState(ST_INVASION);
     }
     else if (presence) {
-        return SystemSetState(ST_MOTION);
+        SystemSetState(ST_MOTION);
     }
     else if (vibration) {
-        return SystemSetState(ST_SHOCK);
+        SystemSetState(ST_SHOCK);
     }
     return;
 }
